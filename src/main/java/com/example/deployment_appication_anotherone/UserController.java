5d26c49f6e1d6209c57dd6b099198563227a974c
~~Agent_freddie~~🔫 <fareedtijani2810@gmail.com>
2024-11-23 12:16:04 +0100

    updated the method

--- src/main/java/com/example/deployment_appication_anotherone/UserController.java
+++ src/main/java/com/example/deployment_appication_anotherone/UserController.java
@@ -5,11 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
 
+Map<String, String> users = new HashMap<>();
     @GetMapping
     public String HelloWorld(){
         return "Hello freddie";}
+
+    public void buildUsers(){
+        users.put("fareed", "olamide");
+        users.put("baby", "boi");
+    }
+
+    @GetMapping("goods")
+    public Map<String, String> good(){
+        buildUsers();
+        return users;
+    }
+
 }
+
+
